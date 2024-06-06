1935307daeb45bcae83542aad7130dbea1c63f3f
Teoramites <teoramites@gmail.com>
2024-06-06 17:39:01 -0300

    suspense needed for deploy

--- property-app/src/app/dashboard/properties/page.tsx
+++ property-app/src/app/dashboard/properties/page.tsx
@@ -1,12 +1,15 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import  Search  from '@/app/ui/dashboard/search';
 import { CreateProperty } from '@/app/ui/properties/buttons';
+import { Suspense } from 'react';
 
 export default function Properties() {
     return (
         <div>
             <div className="my-4 flex content-center justify-center items-center gap-5">
-                <Search placeholder="Search properties..." />
+                <Suspense fallback={<div>Loading...</div>}>    
+                    <Search placeholder="Search properties..." />
+                </Suspense>
                 <CreateProperty />
             </div>
             {/* Send properties as an array and build all cards based on that array */}

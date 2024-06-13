29f237cdcadaa3a6899875f867f35431409e7d85
Teoramites <teoramites@gmail.com>
2024-06-12 21:09:38 -0300

    Solved issue of semicolons

--- property-app/src/app/ui/properties/calendar.tsx
+++ property-app/src/app/ui/properties/calendar.tsx
@@ -1,13 +1,11 @@
-"use client"
+"use client";
 
 import { eachDayOfInterval, endOfMonth, format, getDay, startOfMonth } from "date-fns";
 
-//weekdays in spanish
+// weekdays in Spanish
 const WEEKDAYS = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
 
-
 export default function PropertyCalendar() { 
-    
     const currentDate = new Date();
     const firstDayOfMonth = startOfMonth(currentDate);
     const lastDayOfMonth = endOfMonth(currentDate);
@@ -16,7 +14,7 @@ export default function PropertyCalendar() {
         start: firstDayOfMonth,
         end: lastDayOfMonth
     });
-    
+
     const startingDayIndex = getDay(firstDayOfMonth);
 
     return ( 
@@ -25,26 +23,19 @@ export default function PropertyCalendar() {
                 <h2 className="text-center">{format(currentDate, 'MMMM yyyy')}</h2>
             </div>
             <div className="grid grid-cols-7 gap-2">
-                {WEEKDAYS.map((day) => {
-                    return (
-                        <div key={day} className="text-center border font-bold">
-                            {day}
-                        </div>
-                    );
-                })};
-                {Array.from({ length: startingDayIndex }).map((_, index) => {
-                    return (
-                        <div key={`empty-${index}`} className="text-center border rounded-md p-2 h-20"/>
-                    );
-                })
-                }
-                {daysInMonth.map((day, index) => {
-                    return (
-                        <div key={index} className="text-center border rounded-md p-2 hover:bg-black hover:text-white h-20">
-                            {format(day, 'd')}
-                        </div>
-                    );
-                })};
+                {WEEKDAYS.map((day) => (
+                    <div key={day} className="text-center border font-bold">
+                        {day}
+                    </div>
+                ))}
+                {Array.from({ length: startingDayIndex }).map((_, index) => (
+                    <div key={`empty-${index}`} className="text-center border rounded-md p-2 h-20" />
+                ))}
+                {daysInMonth.map((day, index) => (
+                    <div key={index} className="text-center border rounded-md p-2 hover:bg-black hover:text-white h-20">
+                        {format(day, 'd')}
+                    </div>
+                ))}
             </div>
         </div>
     ); 

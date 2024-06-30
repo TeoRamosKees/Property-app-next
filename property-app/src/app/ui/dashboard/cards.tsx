e51f0dc732dd5f50feb1c91760ff19de8187a5f9
Teoramites <teoramites@gmail.com>
2024-06-30 20:11:30 -0300

    feat better property card

--- property-app/src/app/ui/dashboard/cards.tsx
+++ property-app/src/app/ui/dashboard/cards.tsx
@@ -3,19 +3,26 @@
 import { deleteProperty } from '@/app/actions';
 import { lusitana } from '../fonts';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 const handleDeleteButton = (id: string) => {
     deleteProperty(id);
 };
-  
-export function Card({
-        title,
-        value,
-    }: {
-        title: string;
-        value: number | string;
-    }) {
+const handleEditButton = ({id, router}: {id:string, router: AppRouterInstance}) => {
+    // redirect to edit page
+    router.push(`/dashboard/properties/${id}/edit`);
+};
 
+export function Card({
+    title,
+    value,
+}: {
+    title: string;
+    value: number | string;
+}) {
+    
+    
     return (
         // <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
         // <div className="flex p-4">
@@ -45,24 +52,25 @@ export default function PropertyCard({
     title: string;
     id: string;
 }) {
+    const router = useRouter();
     return (
-        <div className="property-card mb-10">
+        <div className="property-card mb-10 bg-white border border-gray-200 rounded-lg block max-w-sm shadow dark:bg-gray-800 dark:border-gray-700">
             <Link
                 href={`properties/${id}/calendar`}
                 className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
             >
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">ID: {id}</p>
-                <Link
-                    href={`properties/${id}/edit`}
-                    className="bg-black text-white rounded-md p-2 hover:bg-slate-900 m-5"
-                >
-                    Edit
-                </Link>
-                <button className='bg-black text-white rounded-md p-2 hover:bg-slate-900 m-2' onClick={() => handleDeleteButton(id)}>
-                    Delete
-                </button>
+                {/* <p className="font-normal text-gray-700 dark:text-gray-400">ID: {id}</p> */}
             </Link>
+            <div className='grid grid-cols-2'>
+
+                <button className="bg-black text-white rounded-md p-2 hover:bg-slate-900 m-2 flex flex-col items-center" onClick={() => handleEditButton({id, router})}>
+                    Editar
+                </button>
+                <button className="bg-black text-white rounded-md p-2 hover:bg-slate-900 m-2 flex flex-col items-center" onClick={() => handleDeleteButton(id)}>
+                    Eliminar
+                </button>
+            </div>
         </div>
     );
 }
